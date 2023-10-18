38859dd937c53ad0b5c3cb5f7d8925b233a0797b
msander2019 <53744416+msander2019@users.noreply.github.com>
2023-10-18 14:29:27 +0300

    create Notification service for RabbitMQ v2

---
+++ notification-service/src/main/java/ua/kyiv/mesharea/notification/service/DepositMessageHandler.java
@@ -0,0 +1,46 @@
+package ua.kyiv.mesharea.notification.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import ua.kyiv.mesharea.notification.config.RabbitMQConfig;
+
+// обработка сообщений которые приходят из RabbitMQ (consumer)
+@Service
+public class DepositMessageHandler {
+
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    public DepositMessageHandler(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_DEPOSIT)
+    public void receive(Message message) throws JsonProcessingException {
+        System.out.println(message); // прийдет в байтовом виде
+        byte[] body = message.getBody();
+        String jsonBody = new String(body);
+        ObjectMapper objectMapper = new ObjectMapper();
+        DepositResponseDTO depositResponseDTO = objectMapper.readValue(jsonBody, DepositResponseDTO.class);
+        System.out.println(depositResponseDTO);
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(depositResponseDTO.getMail());
+        mailMessage.setFrom("alex@gmail.com");
+
+        mailMessage.setSubject("Deposit");
+        mailMessage.setText("Make deposit, sum: " + depositResponseDTO.getAmount());
+
+        try {
+            javaMailSender.send(mailMessage);
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+    }
+}

97e5b687d39ab9a2b87e1072a9eb96726da7ec8a
msander2019 <53744416+msander2019@users.noreply.github.com>
2023-10-18 11:12:17 +0300

    create Config service

--- config-service/src/main/java/ua/kyiv/mesharea/config/ConfigApplication.java
+++ config-service/src/main/java/ua/kyiv/mesharea/config/ConfigApplication.java
@@ -1,7 +1,14 @@
 package ua.kyiv.mesharea.config;
 
-public class ConfigApplication {
-    public static void main(String[] args) {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
     }
 }
